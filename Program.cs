f6aef64f29370ed6625821ea7f7f9f3317443e8c
Thomas Stanger <thomasstanger0@gmail.com>
2020-11-17 12:29:44 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int length = 5;
+            var inputQueue = new string[length];
+            var queue = new Queue(inputQueue, length);
+           
+            queue.Enqueue("1");
+            queue.Enqueue("2");
+            queue.Enqueue("3");
+            queue.Enqueue("4");
+            queue.Enqueue("5");
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+
+        }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+
+namespace Queues
+{
+    public class Queue
+    {
+        private int _count;
+        private int _max;
+        private int _headPointer;
+        private int _tailPointer = -1;
+        public string[] queue;
+
+        public Queue(string[] Queue, int length)
+        {
+            queue = Queue;
+            _max = length;
+        }
+
+        public void Enqueue(string item)
+        {
+            if (_count == _max)
+            {
+                Console.WriteLine("Queue Overflow");
+            }
+                
+            else
+            {
+                _tailPointer = (_tailPointer +1) % _max;
+                queue[_tailPointer] = item;
+
+                _count++;
+            }
+        }
+
+        public void Dequeue()
+        {
+            if (_count == 0)
+            {
+                Console.WriteLine("Queue Underflow");
+            }
+
+            else
+            {
+                var output = queue[_headPointer];
+                queue[_headPointer] = null;
+                _headPointer = (_headPointer + 1) % _max;
+
+                _count--;
+                Print(output);
+            }
+        }
+
+        private void Print(string output)
+        {
+            Console.WriteLine(output);
+        }
+    }
+}
